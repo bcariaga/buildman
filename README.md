c9c68c0f32b987520ee520dbc8d82436715c2b0c
bcariaga <cariaga.bh@gmail.com>
2018-09-12 23:21:41 -0300

    agrego sources y datos para cli :monkey:

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # buildman
 A tool for making files from a Postman Collection and vice versa
+
+## Uso:
+
+Actualmente cuenta con dos comandos:
+
+**import** : recibe el path de la collection de postman (JSON) y el target path donde se va a escribir la estructura de carpetas con la info leida de las colleciones.:
+
+>`buildman import "path/to/collection.json" "path/to/target/folder"`
+
+**export** : lee un directorio* y crea una coleccion de postman (JSON).
+
+>`buildman export "path/to/collection/folder" "path/to/save/collection"`
+
+*el **directorio** debe tener cierto formato (el mismo que se crea al leer una colección), en cada nivel se puede definir un test.js y un prescript.js, en el caso de ser el primer nivel o el utimo se agrega un definition.json, que contiene informacion de la collection como el nombre (y nose que mas) y en el caso del request tiene la url, el tipo de llamada etc...
+
+**Adicionalmente:** Si se llama sin parámetros se va a intentar hacer un export (leer un directorio y crear una postman collection), para que esto funcione la raiz del directorio debe tener un archivo json con el siguiente nombre: `buildman.json` y el siguiente formato:
+
+```json
+{
+    "collection-folder" : "",
+    "destination-path" : ""
+}
+```
+donde *collection-folder* es la raiz donde se encuentra la coleccion de postman (archivos) y *destination-path* es el lugar donde se va a escribir el JSON
--- index.js
+++ index.js
@@ -1,16 +1,83 @@
 #!/usr/bin/env node
 
+const   Spinner = require('cli-spinner').Spinner,
+        importCollection = require("./lib/import").importCollection,
+        exportCollection = require("./lib/export").exportCollection,
+        fs = require('fs');
+
+const validateFiles = (paths = []) =>{
+    paths.map(path => {
+        if (!fs.existsSync(path)) { 
+            console.log(`la ruta ${path} no es valida..`);
+            process.exit(1);
+        } 
+    })
+}
+const printInfo = () =>{
+    console.log("Hola! gracias por usar buildman! \n");
+}
+
 const program = require('commander');
 
 program
-  .version('0.1.0')
-  .option('-i, --import <collection>', 'Import a Postman Collection from JSON', (col) => {
-      console.log("importando collection... " + col);
-      
-  })
-  .option('-e, --export <path>', 'Export a from file system to Postman Collection (JSON)', (path) => {
-      console.log("exportando coleccion " + path);
-      
-  })
-  .parse(process.argv)
-  .name("buildman");
+    .command('import <collection> <target>')
+    .action(function(collection, target) {
+        printInfo();
+        console.log(collection);
+        console.log(target);
+        let spinner = new Spinner(`Importando ${collection} `);
+        validateFiles([collection, target]);
+        spinner.setSpinnerString(18);
+        spinner.start();
+        try {
+            importCollection(collection, target);
+            console.log("coleccion importada!");
+            
+        } catch (error) {
+            console.log("ups! algo se rompio! \n");
+            console.log(error);
+        }finally{
+            spinner.stop(true)
+        }
+    })
+    .description("lee una postman collection (JSON) y crea una copia en archivos");
+
+program
+    .command('export <collection> <target>')
+    .action(function(collection, target) {
+        printInfo();
+        let spinner = new Spinner(`Exportando ${collection} `);
+        validateFiles([collection, target]);
+        spinner.setSpinnerString(18);
+        spinner.start();
+        try {
+            exportCollection(collection, target);
+            console.log("coleccion exportada!");
+        } catch (error) {
+            console.log("ups! algo se rompio! \n");
+            console.log(error);
+        }finally{
+            spinner.stop(true)
+        }
+    })
+    .description("lee un directorio y crea una postman collection");
+
+program.version('0.1.0');
+program.name("buildman")
+program.parse(process.argv);
+    
+if (program.args.length < 1 ) {
+    // console.log(__dirname);
+    let config;
+    try {
+        config = JSON.parse(fs.readFileSync(`${__dirname}/buildman.json`));
+        validateFiles([config['collection-folder'], config['destination-path']]);
+        console.log(config['collection-folder']);
+        console.log(config['destination-path']);
+        
+        exportCollection(config['collection-folder'], config['destination-path']);
+    } catch (error) {
+        console.log("ups!");
+        console.error(error);
+    }
+}
