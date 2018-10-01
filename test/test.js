7cbbadfd7166e22b7e674cb4d82b0a64e15ef776
bcariaga <cariaga.bh@gmail.com>
2018-09-30 23:30:48 -0300

    cleanup

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const   importCollectionTests = require('./lib/import'),
-        exportCollectionTests = require('./lib/export'); 
-
-const runAll = () => {
-    importCollectionTests.run();
-    exportCollectionTests.run();
-}
-
-runAll();
