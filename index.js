c648a897336a71c05752d11b57c9c9b08dd62cf6
bcariaga <cariaga.bh@gmail.com>
2018-10-02 00:41:19 -0300

    paso librerias funciones a modulo aparte :ciclon:

--- index.js
+++ index.js
@@ -1,68 +1,39 @@
 #!/usr/bin/env node
 
-const  importCollection = require("./lib/import").importCollection,
+const   importCollection = require("./lib/import").importCollection,
         exportCollection = require("./lib/export").exportCollection,
-        fs = require('fs')
-        CFonts = require('cfonts');
+        utils = require('./lib/app/utils');
 
-const validateFiles = (paths = []) =>{
-    paths.map(path => {
-        if (!fs.existsSync(path)) { 
-            say(`la ruta ${path} no es valida..`, "yellowBright");
-            process.exit(1);
-        } 
+const messageColors = 
+{
+    success : "green",
+    warning : "yellow",
+    danger : "red",
+    info : "cyan"
+}
+
+const program = require('commander');
+const preCommand = (collection, target) =>{
+    utils.welcome();
+    utils.validateFiles([collection, target], validationRules => {
+        validationRules.map(rule => {
+            if (!rule.valid) utils.say(`opa! ${rule.message}`, messageColors.danger);
+        });
+        if (validationRules.some(rule => !rule.valid)) process.exit(1); 
     })
 }
-const printInfo = () =>{
-    CFonts.say('buildman', {
-        font: 'block', 
-        align: 'center',
-        colors: ['redBright', 'yellowBright'],
-        background: 'transparent',
-        letterSpacing: 0,
-        lineHeight: 1,
-        space: true,
-        maxLength: '0',
-    });
-    CFonts.say('buildman v0.1(beta)', {
-        font: 'console', 
-        align: 'right',
-        colors: ['cyan'],
-        background: 'transparent',
-        letterSpacing: 0,
-        lineHeight: 1,
-        space: true,
-        maxLength: '0',
-    });
-    // console.info("buildman v0.1(beta) \n");
-}
-
-const say = (msg, color = 'system')=>{
-    CFonts.say(msg, {
-        font: 'console', 
-        align: 'left',
-        colors: [color],
-        background: 'transparent',
-        letterSpacing: 0,
-        lineHeight: 1,
-        space: true,
-        maxLength: '0',
-    });
-}
-const program = require('commander');
 
 program
     .command('import <collection> <target>')
     .action(function(collection, target) {
-        printInfo();
-        validateFiles([collection, target]);
+        preCommand(collection, target);
         
         try {
             importCollection(collection, target);
-            say("coleccion importada!", "green");
+            utils.say(`coleccion importada en ${target}`, messageColors.success);
         } catch (error) {
-            say("ups! algo se rompio! \n", "red");
-            console.log(error);
+            utils.say(`ups! algo se rompio! \n`, messageColors.danger);
+            // console.log(error);
         }
     })
     .description("lee una postman collection (JSON) y crea una copia en archivos");
@@ -70,15 +41,13 @@ program
 program
     .command('export <collection> <target>')
     .action(function(collection, target) {
-        printInfo();
-        validateFiles([collection, target]);
-        
+        preCommand(collection, target);
         try {
             exportCollection(collection, target);
-            say("coleccion exportada!", "green");
+            utils.say(`coleccion exportada en ${target}`, messageColors.success);
         } catch (error) {
-            say("ups! algo se rompio! \n", "red");
-            console.log(error);
+            utils.say(`ups! algo se rompio! \n`, messageColors.danger);
+            //console.log(error);
         }
     })
     .description("lee un directorio y crea una postman collection");
@@ -88,16 +57,15 @@ program.name("buildman")
 program.parse(process.argv);
     
 if (program.args.length < 1 ) {
-    // console.log(__dirname);
     printInfo();
     try {
         say("holo!", "red")
         let config = JSON.parse(fs.readFileSync(`${__dirname}/buildman.json`));
-        validateFiles([config['collection-folder'], config['destination-path']]);
+        preCommand(config['collection-folder'], config['destination-path']);
         exportCollection(config['collection-folder'], config['destination-path']);
-        say("coleccion exportada!", "green");
+        utils.say(`coleccion importada en ${config['collection-folder']}`, messageColors.success);
     } catch (error) {
-        say("ups! algo se rompio! \n", "red");
-        console.log(error);
+        utils.say(`ups! algo se rompio! \n`, messageColors.danger);
+        //console.log(error);
     }
 }
