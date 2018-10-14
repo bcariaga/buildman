ce0a69553e90a263642e7ff47bfea18512e929b6
bcariaga <cariaga.bh@gmail.com>
2018-10-13 23:43:04 -0300

    more tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 const   importCollectionTests = require('./lib/import'),
-        exportCollectionTests = require('./lib/export'); 
+        exportCollectionTests = require('./lib/export'),
+        utilsTests = require('./lib/utils'); 
 
 const runAll = () => {
     importCollectionTests.run();
     exportCollectionTests.run();
+    utilsTests.run();
 }
 
 runAll();
